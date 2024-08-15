95cd70f618642a3ab5555752b2139ebdb4474e68
Jainam Chhatbar <jnchhatbar.jnc@gmail.com>
2024-08-15 00:59:47 -0700

    mapreduce: all tests passing

--- src/mr/rpc.go
+++ src/mr/rpc.go
@@ -6,8 +6,10 @@ package mr
 // remember to capitalize all names.
 //
 
-import "os"
-import "strconv"
+import (
+	"os"
+	"strconv"
+)
 
 //
 // example to show how to declare the arguments
@@ -24,6 +26,44 @@ type ExampleReply struct {
 
 // Add your RPC definitions here.
 
+type MapTask struct {
+	InputFile     string
+	MapTaskNumber int
+	NReducer      int
+}
+
+type ReduceTask struct {
+	IntermediateFiles []string
+	ReduceTaskNumber  int
+}
+
+type GetTaskArgs struct {
+}
+
+type GetTaskReply struct {
+	MapTask    *MapTask
+	ReduceTask *ReduceTask
+	Done       bool
+}
+
+type MapTaskResponse struct {
+	InputFile         string
+	IntermediateFiles []string
+	Error             bool
+}
+
+type ReduceTaskResponse struct {
+	ReduceTaskNumber int
+	Error            bool
+}
+
+type PerformTaskArgs struct {
+	MapTaskResponse    *MapTaskResponse
+	ReduceTaskResponse *ReduceTaskResponse
+}
+
+type PerformTaskReply struct {
+}
 
 // Cook up a unique-ish UNIX-domain socket name
 // in /var/tmp, for the coordinator.
